0ebcd29362ba0900da70e6836e418d0a49e21773
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-19 08:14:00 -0400

    Initial Commit

---
+++ SwiftUIViewPractice/SwiftUIViewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewPracticeApp.swift
+//  SwiftUIViewPractice
+//
+//  Created by Matthew Garlington on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
