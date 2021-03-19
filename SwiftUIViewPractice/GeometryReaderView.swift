1ca6b6b58e032cf33b9985596615215c7403a0a9
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-19 10:19:23 -0400

    init commit

---
+++ SwiftUIViewPractice/GeometryReaderView.swift
@@ -0,0 +1,20 @@
+//
+//  GeometryReaderView.swift
+//  SwiftUIViewPractice
+//
+//  Created by Matthew Garlington on 3/19/21.
+//
+
+import SwiftUI
+
+struct GeometryReaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GeometryReaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReaderView()
+    }
+}
