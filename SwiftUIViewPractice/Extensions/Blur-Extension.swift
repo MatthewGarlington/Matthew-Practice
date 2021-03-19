f3fa030e55350dae790942215433da81dfcca0b4
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-19 09:19:42 -0400

    init commit

---
+++ SwiftUIViewPractice/Extensions/Blur-Extension.swift
@@ -0,0 +1,39 @@
+//
+//  Blur-Extension.swift
+//  SwiftUIViewPractice
+//
+//  Created by Matthew Garlington on 3/19/21.
+//
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    
+    typealias UIViewType = UIView
+    var style: UIBlurEffect.Style
+ 
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: CGRect.zero)
+        view.backgroundColor = .clear
+        
+        // adding the style variable makes the blur style customizable
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+       
+        view.insertSubview(blurView, at: 0)
+        
+        NSLayoutConstraint.activate([
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+}
---
+++ SwiftUIViewPractice/ZStackView.swift
@@ -0,0 +1,20 @@
+//
+//  ZStackView.swift
+//  SwiftUIViewPractice
+//
+//  Created by Matthew Garlington on 3/19/21.
+//
+
+import SwiftUI
+
+struct ZStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ZStackView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStackView()
+    }
+}
