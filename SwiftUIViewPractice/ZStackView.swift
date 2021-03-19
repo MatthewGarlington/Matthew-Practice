906c74b11b32128b80d5d19803b8ee03e957a0fb
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-19 09:21:41 -0400

    feat: Added view that presents ZStack
    This added view shows the use of depth stacks (ZStacks) that present views on top of other views. The text displays on top of a zstack that is the rounded rectangle with a 40% opacity

--- SwiftUIViewPractice/ZStackView.swift
+++ SwiftUIViewPractice/ZStackView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct ZStackView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .top) {
+            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)), Color.white]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack {
+                
+                
+                ZStack(alignment: .top) {
+                    Spacer()
+                        .frame(width: 375, height: 300)
+                        .background(Color.white.opacity(0.4))
+                        .clipShape(RoundedRectangle(cornerRadius: 25.0, style: .continuous))
+                    
+                    VStack(spacing: 20) {
+                        Text("Depth (Z) Stacks")
+                            .bold()
+                            .font(.title)
+                        Text("This view shows the use of ZStacks with a while 40% see through rounded rectangle. Notice how the views are stacked on top of one another including the Text view.")
+                            .font(.headline)
+                         
+                    }
+                    .padding(30)
+                    
+                }
+                
+            }
+        }
     }
 }
 
