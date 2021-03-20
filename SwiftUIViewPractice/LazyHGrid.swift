29cc4e1432916f097fa98cc5e5bc3dafd12bfd3e
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-20 08:40:06 -0400

    Added file to show LazyHGrid

---
+++ SwiftUIViewPractice/LazyHGrid.swift
@@ -0,0 +1,20 @@
+//
+//  LazyHGrid.swift
+//  SwiftUIViewPractice
+//
+//  Created by Matthew Garlington on 3/20/21.
+//
+
+import SwiftUI
+
+struct LazyHGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LazyHGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        LazyHGrid()
+    }
+}
