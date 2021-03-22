285a9ecf2b9e04650f428e0fed11cf1879fc72c1
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-22 07:05:44 -0400

    Init commit of ScrollView Reader

---
+++ SwiftUIViewPractice/ScrollViewReader.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollViewReader.swift
+//  SwiftUIViewPractice
+//
+//  Created by Matthew Garlington on 3/21/21.
+//
+
+import SwiftUI
+
+struct ScrollViewReader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrollViewReader_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewReader()
+    }
+}
