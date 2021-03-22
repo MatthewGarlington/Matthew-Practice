4b2457a6c8759ab0119c5d81a24a15b69e89f661
Matthew Garlington <SBMatthewG@gmail.com>
2021-03-22 08:22:22 -0400

    Added LazyHGrid View
    This view shows exampels of how Lazy HGrids can be used in SwiftUI to create a grid that can have custom spacing and layout

--- SwiftUIViewPractice/LazyHGrid.swift
+++ SwiftUIViewPractice/LazyHGrid.swift
@@ -7,14 +7,50 @@
 
 import SwiftUI
 
-struct LazyHGrid: View {
+struct LazyHGrid_RowAndColumnSpacing: View {
+    @State private var columnSpacing: CGFloat = 10
+    @State private var rowSpacing: CGFloat = 20
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("LazyHGrid",
+                       subtitle: "Spacing Example",
+                       desc: "This is just to give you an idea of controlling column and row spacing.")
+            
+            let rows = [GridItem(.fixed(40), spacing: rowSpacing),
+                        GridItem(.fixed(40), spacing: rowSpacing),
+                        GridItem(.fixed(40))]
+            
+            LazyHGrid(rows: rows, spacing: columnSpacing) {
+                ForEach(1 ..< 11) { item in
+                    Color.green
+                        .frame(width: 40, height: 40)
+                }
+            }
+            
+            VStack {
+                Slider(value: $columnSpacing, in: 0...40, step: 5,
+                       minimumValueLabel: Text("0"),
+                       maximumValueLabel: Text("40")) { Text("Minimum Spacing")}
+                Text("Column Spacing: \(Int(columnSpacing))")
+                    .padding(.bottom)
+                
+                Slider(value: $rowSpacing, in: 0...40, step: 5,
+                       minimumValueLabel: Text("0"),
+                       maximumValueLabel: Text("40")) { Text("Minimum Spacing")}
+                Text("Row Spacing: \(Int(rowSpacing))")
+            }
+            .padding(.horizontal)
+        }
+        .font(.title)
     }
 }
 
-struct LazyHGrid_Previews: PreviewProvider {
+struct LazyHGrid_RowAndColumnSpacing_Previews: PreviewProvider {
     static var previews: some View {
-        LazyHGrid()
+        LazyHGrid_RowAndColumnSpacing()
     }
 }
+
+
+
